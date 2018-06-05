16f5893f866d8421bdaa017fc8335a7d0f073696
M Jesús Otero <mjesuotero@Maria-Jesus-Otero-R-T.local>
2018-06-04 19:29:32 -0700

    x

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,69 +4,60 @@ var path = require("path");
 var app = express();
 
 // Pull in our data that we want to display on our API route
-var friendsData = require("../data/friends-data.js");
+var friendsData = require("../data/friends-data.json");
+console.log(friendsData);
 
 
 module.exports = function(app) {
 
-    // Display all friends
+    // GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
     app.get("/api/friends", function(req,res) {
-        console.log(friendsData);
+        // console.log(friendsData);
         return res.json(friendsData);
     });
 
-    //Create a new friend – push it into the array
+    // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
     app.post("/api/friends", function(req, res) {
         // req.body hosts is equal to the JSON post sent from the user to the server
         // This works because of our body-perser middleware
         var newfriend = req.body;
-
-        // Useing a RexEx Pattern to remove spaces from newCharacter
-        // newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
         console.log(newfriend);
 
-        //QUESTION!!! How I can push the new info in the friendsArr that is inside /data/friends-data??
-        friendsData.push(newfriend);
 
+
+        // Comparation code
+        // Compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDiff`
+            // * Example: 
+                // * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+                // * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+                // * Total Difference: **2 + 1 + 2 =** **_5_**
+    
+        var match;
+        var matchScore = 100;
+        for (var i = 0; i < friendsData.length; i++) {
+            // Looping through friends objects
+            var totalDiff = 0;
+            // Looping through score arrays
+            for (var x = 0; x < 10; x++) {
+                //Use the absolute value of the differences
+                totalDiff += Math.abs(newfriend.scores[x] - friendsData[i].scores[x])
+            }
+            // Match!
+            // * The closest match will be the user with the least amount of difference.
+            if (totalDiff < matchScore) {
+                matchScore = totalDiff;
+                match = friendsData[i];
+            }
+        }
+
+        // Puch the new friend into de friends-data objet array
+        friendsData.push(newfriend);
         return res.json(newfriend);
-    });
         
+        
+        console.log(match);
+    });      
 }
 
 
-// 4. Your `apiRoutes.js` file should contain two routes:
-//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
 
-
-// 6. Determine the user's most compatible friend using the following as a guide:
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example: 
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
-//    * The closest match will be the user with the least amount of difference.
-
-
-
-
-//   // Display a single character, or return false
-//   app.get("/api/:characters?", function(req, res) {
-//     var chosen = req.params.characters;
-  
-//     // Chosen = true, osea if chosen es creado...
-//     if (chosen) {
-//       console.log(chosen);
-//       for(var i = 0; i < characters.length; i++) {
-//         if (chosen === characters[i].routeName) {
-//           res.json(characters[i]);
-//           return;
-//         }
-//       }
-//       res.send("No character found!");
-//     } else {
-//       res.json(characters);
-//     }
-//   });
--- server.js
+++ server.js
@@ -13,20 +13,13 @@ var PORT = process.env.PORT || 7080;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
+
 // parse application/json
 app.use(bodyParser.json())
 
 
  
 // ROUTES
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, '/app/public/home.html'));
-// });
-
-// app.get('/survey', function(req, res) {
-//     res.sendFile(path.join(__dirname, '/app/public/survey.html'));
-// });
-
 // Including html routes in the server file // Our external route
 require("./app/routing/api-routes.js")(app);
 require("./app/routing/html-routes.js")(app);
